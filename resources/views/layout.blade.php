d810200f5d24482fabfa1821cfa827f351b63f32
TuriniMattia <turinimattia96@gmail.com>
2024-05-21 17:55:16 +0200

    [add]struttura_base

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1>creazione homepage</h1>
+    @include('partials.header')
+
+    <div class="content">
+        @yield('content')
+    </div>
+
+    @include('partials.footer')
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
